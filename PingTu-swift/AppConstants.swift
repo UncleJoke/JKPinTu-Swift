467d7af4c41a852e1e23021c5206eed40bf43c71
bingjie <bingjiesh@foxmail.com>
2015-12-09 13:59:36 +0800

    加上放荡不羁的Logo

--- PingTu-swift/AppConstants.swift
+++ PingTu-swift/AppConstants.swift
@@ -14,3 +14,11 @@ let SCREEN_WIDTH  = UIScreen.mainScreen().bounds.width
 let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.height
 
 let BOTTOMHEIGHT  = 49
+
+
+///** DEBUG LOG **/
+//#ifdef DEBUG
+//#define DLog( s, ... ) NSLog( @"<%@:(%d)>: %@", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:(s), ##__VA_ARGS__] )
+//#else
+//#define DLog( s, ... )
+//#endif
